a1f7ecd583e53c5d981622b845f28c0c6c56f65e
banna12345bob <banna12345bob@outlook.com>
2022-08-31 19:13:27 +1000

    added application class for interfacing with applications

--- engine/engine.py
+++ engine/engine.py
@@ -4,4 +4,23 @@ from engine.commands import commands
 from engine.inventory import inventory
 from engine.FileRead import File
 
-# Hello this is just a interface so that the main program can use all the engine functions without importing them directly
+# This class is for all applications that will be run with the engine
+class application:
+    def __init__(self, area, room):
+        self.area = area
+        self.room = room
+    
+    def commandRun(self, inp):
+        giveCommand = commands(self.area, self.room).giveCommand(inp)
+        # For this design if a function returns 0 it means that it didn't work
+        if giveCommand != 0:
+            if giveCommand != "Unknown command" and isinstance(giveCommand, list):
+                if interpreter.room(giveCommand[0], giveCommand[1]).getRoomName() != 0:
+                    print(interpreter.room(giveCommand[0], giveCommand[1]).getRoomName())
+                    return giveCommand
+                else:
+                    return ["1","1"]
+            else:
+                return giveCommand
+        else:
+            return ["1","1"]
--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 from engine import engine
 
 # Please not this only works when one installs colorama I would recommended running via launch.bat
-room = 1
 area = 1
+room = 1
 print(engine.interpreter.room(area, room).getRoomName())
 inp = input("Command: ")
+# This interfaces with application I want to have each indivial application inheritant from engine.application
 while inp:
     try:
-        giveCommand = engine.commands(area, room).giveCommand(inp)
-        # For this design if a function returns 0 it means that it didn't work
-        if giveCommand != 0:
-            if giveCommand != "Unknown command" and isinstance(giveCommand, list):
-                if engine.interpreter.room(giveCommand[0], giveCommand[1]).getRoomName() != 0:
-                    print(engine.interpreter.room(giveCommand[0], giveCommand[1]).getRoomName())
-                    area = giveCommand[0]
-                    room = giveCommand[1]
-                else:
-                    area = 1
-                    room = 1
-            else:
-                print(giveCommand)
-        else:
-            area = 1
-            room = 1
-        if giveCommand == "quit":
+        app = engine.application(area, room).commandRun(inp)
+        if app == 0:
             break
+        else:
+            if isinstance(app, list):
+                area = app[0]
+                room = app[1]
+            else:
+                if app != "quit":
+                    print(app)
+                else:
+                    break
         inp = input("Command: ")
     except:
-        engine.debugger.fatal("UNCAUGHT ERROR")
+        engine.debugger().fatal("UNCAUGHT ERROR")

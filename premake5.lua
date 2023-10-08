6e02a73a561d3d9e0c2055956a4ca0ad21e9a3a4
banna12345bob <59811283+banna12345bob@users.noreply.github.com>
2023-10-08 17:25:11 +1100

    Removed deprecate function

--- premake5.lua
+++ premake5.lua
@@ -23,7 +23,7 @@ function getPythonLib()
     return queryTerminal("python -c \"import sys; import os; import glob; path = os.path.dirname(sys.executable); libs = glob.glob(path + '/libs/python*'); print(os.path.splitext(os.path.basename(libs[-1]))[0]);\"")
 end
 
-if os.get() == "windows" then
+if os.host() == "windows" then
     pythonPath      = getPythonPath()
     pythonIncludePath = pythonPath .. "/../include/*"
     pythonLibPath     = pythonPath .. "/../lib/"
